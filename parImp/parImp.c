45e65f2142e8ea2990e128020123d04fe08b840f
gmaayan <gabemgem@gmail.com>
2018-06-15 14:35:37 -0500

    Replaced string lookup table with switch statement

--- parImp/parImp.c
+++ parImp/parImp.c
@@ -23,84 +23,84 @@
 void error_handler(cl_int err, char* message) {
    if(err == CL_SUCCESS)
       return;
-   static const char* strings[] =
-   {
-      // Error Codes
-      "CL_SUCCESS"                                  //   0
-      , "CL_DEVICE_NOT_FOUND"                         //  -1
-      , "CL_DEVICE_NOT_AVAILABLE"                     //  -2
-      , "CL_COMPILER_NOT_AVAILABLE"                   //  -3
-      , "CL_MEM_OBJECT_ALLOCATION_FAILURE"            //  -4
-      , "CL_OUT_OF_RESOURCES"                         //  -5
-      , "CL_OUT_OF_HOST_MEMORY"                       //  -6
-      , "CL_PROFILING_INFO_NOT_AVAILABLE"             //  -7
-      , "CL_MEM_COPY_OVERLAP"                         //  -8
-      , "CL_IMAGE_FORMAT_MISMATCH"                    //  -9
-      , "CL_IMAGE_FORMAT_NOT_SUPPORTED"               //  -10
-      , "CL_BUILD_PROGRAM_FAILURE"                    //  -11
-      , "CL_MAP_FAILURE"                              //  -12
+   char* error_message = "";
+   switch(err){
+    // run-time and JIT compiler errors
+    case 0: error_message = "CL_SUCCESS";
+    case -1: error_message = "CL_DEVICE_NOT_FOUND";
+    case -2: error_message = "CL_DEVICE_NOT_AVAILABLE";
+    case -3: error_message = "CL_COMPILER_NOT_AVAILABLE";
+    case -4: error_message = "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+    case -5: error_message = "CL_OUT_OF_RESOURCES";
+    case -6: error_message = "CL_OUT_OF_HOST_MEMORY";
+    case -7: error_message = "CL_PROFILING_INFO_NOT_AVAILABLE";
+    case -8: error_message = "CL_MEM_COPY_OVERLAP";
+    case -9: error_message = "CL_IMAGE_FORMAT_MISMATCH";
+    case -10: error_message = "CL_IMAGE_FORMAT_NOT_SUPPORTED";
+    case -11: error_message = "CL_BUILD_PROGRAM_FAILURE";
+    case -12: error_message = "CL_MAP_FAILURE";
+    case -13: error_message = "CL_MISALIGNED_SUB_BUFFER_OFFSET";
+    case -14: error_message = "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
+    case -15: error_message = "CL_COMPILE_PROGRAM_FAILURE";
+    case -16: error_message = "CL_LINKER_NOT_AVAILABLE";
+    case -17: error_message = "CL_LINK_PROGRAM_FAILURE";
+    case -18: error_message = "CL_DEVICE_PARTITION_FAILED";
+    case -19: error_message = "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
 
-      , ""    //  -13
-      , ""    //  -14
-      , ""    //  -15
-      , ""    //  -16
-      , ""    //  -17
-      , ""    //  -18
-      , ""    //  -19
-      , ""    //  -20
-      , ""    //  -21
-      , ""    //  -22
-      , ""    //  -23
-      , ""    //  -24
-      , ""    //  -25
-      , ""    //  -26
-      , ""    //  -27
-      , ""    //  -28
-      , ""    //  -29
+    // compile-time errors
+    case -30: error_message = "CL_INVALID_VALUE";
+    case -31: error_message = "CL_INVALID_DEVICE_TYPE";
+    case -32: error_message = "CL_INVALID_PLATFORM";
+    case -33: error_message = "CL_INVALID_DEVICE";
+    case -34: error_message = "CL_INVALID_CONTEXT";
+    case -35: error_message = "CL_INVALID_QUEUE_PROPERTIES";
+    case -36: error_message = "CL_INVALID_COMMAND_QUEUE";
+    case -37: error_message = "CL_INVALID_HOST_PTR";
+    case -38: error_message = "CL_INVALID_MEM_OBJECT";
+    case -39: error_message = "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
+    case -40: error_message = "CL_INVALID_IMAGE_SIZE";
+    case -41: error_message = "CL_INVALID_SAMPLER";
+    case -42: error_message = "CL_INVALID_BINARY";
+    case -43: error_message = "CL_INVALID_BUILD_OPTIONS";
+    case -44: error_message = "CL_INVALID_PROGRAM";
+    case -45: error_message = "CL_INVALID_PROGRAM_EXECUTABLE";
+    case -46: error_message = "CL_INVALID_KERNEL_NAME";
+    case -47: error_message = "CL_INVALID_KERNEL_DEFINITION";
+    case -48: error_message = "CL_INVALID_KERNEL";
+    case -49: error_message = "CL_INVALID_ARG_INDEX";
+    case -50: error_message = "CL_INVALID_ARG_VALUE";
+    case -51: error_message = "CL_INVALID_ARG_SIZE";
+    case -52: error_message = "CL_INVALID_KERNEL_ARGS";
+    case -53: error_message = "CL_INVALID_WORK_DIMENSION";
+    case -54: error_message = "CL_INVALID_WORK_GROUP_SIZE";
+    case -55: error_message = "CL_INVALID_WORK_ITEM_SIZE";
+    case -56: error_message = "CL_INVALID_GLOBAL_OFFSET";
+    case -57: error_message = "CL_INVALID_EVENT_WAIT_LIST";
+    case -58: error_message = "CL_INVALID_EVENT";
+    case -59: error_message = "CL_INVALID_OPERATION";
+    case -60: error_message = "CL_INVALID_GL_OBJECT";
+    case -61: error_message = "CL_INVALID_BUFFER_SIZE";
+    case -62: error_message = "CL_INVALID_MIP_LEVEL";
+    case -63: error_message = "CL_INVALID_GLOBAL_WORK_SIZE";
+    case -64: error_message = "CL_INVALID_PROPERTY";
+    case -65: error_message = "CL_INVALID_IMAGE_DESCRIPTOR";
+    case -66: error_message = "CL_INVALID_COMPILER_OPTIONS";
+    case -67: error_message = "CL_INVALID_LINKER_OPTIONS";
+    case -68: error_message = "CL_INVALID_DEVICE_PARTITION_COUNT";
 
-      , "CL_INVALID_VALUE"                            //  -30
-      , "CL_INVALID_DEVICE_TYPE"                      //  -31
-      , "CL_INVALID_PLATFORM"                         //  -32
-      , "CL_INVALID_DEVICE"                           //  -33
-      , "CL_INVALID_CONTEXT"                          //  -34
-      , "CL_INVALID_QUEUE_PROPERTIES"                 //  -35
-      , "CL_INVALID_COMMAND_QUEUE"                    //  -36
-      , "CL_INVALID_HOST_PTR"                         //  -37
-      , "CL_INVALID_MEM_OBJECT"                       //  -38
-      , "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR"          //  -39
-      , "CL_INVALID_IMAGE_SIZE"                       //  -40
-      , "CL_INVALID_SAMPLER"                          //  -41
-      , "CL_INVALID_BINARY"                           //  -42
-      , "CL_INVALID_BUILD_OPTIONS"                    //  -43
-      , "CL_INVALID_PROGRAM"                          //  -44
-      , "CL_INVALID_PROGRAM_EXECUTABLE"               //  -45
-      , "CL_INVALID_KERNEL_NAME"                      //  -46
-      , "CL_INVALID_KERNEL_DEFINITION"                //  -47
-      , "CL_INVALID_KERNEL"                           //  -48
-      , "CL_INVALID_ARG_INDEX"                        //  -49
-      , "CL_INVALID_ARG_VALUE"                        //  -50
-      , "CL_INVALID_ARG_SIZE"                         //  -51
-      , "CL_INVALID_KERNEL_ARGS"                      //  -52
-      , "CL_INVALID_WORK_DIMENSION"                   //  -53
-      , "CL_INVALID_WORK_GROUP_SIZE"                  //  -54
-      , "CL_INVALID_WORK_ITEM_SIZE"                   //  -55
-      , "CL_INVALID_GLOBAL_OFFSET"                    //  -56
-      , "CL_INVALID_EVENT_WAIT_LIST"                  //  -57
-      , "CL_INVALID_EVENT"                            //  -58
-      , "CL_INVALID_OPERATION"                        //  -59
-      , "CL_INVALID_GL_OBJECT"                        //  -60
-      , "CL_INVALID_BUFFER_SIZE"                      //  -61
-      , "CL_INVALID_MIP_LEVEL"                        //  -62
-      , "CL_INVALID_GLOBAL_WORK_SIZE"                 //  -63
-      , "CL_UNKNOWN_ERROR_CODE"
-   };
+    // extension errors
+    case -1000: error_message = "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
+    case -1001: error_message = "CL_PLATFORM_NOT_FOUND_KHR";
+    case -1002: error_message = "CL_INVALID_D3D10_DEVICE_KHR";
+    case -1003: error_message = "CL_INVALID_D3D10_RESOURCE_KHR";
+    case -1004: error_message = "CL_D3D10_RESOURCE_ALREADY_ACQUIRED_KHR";
+    case -1005: error_message = "CL_D3D10_RESOURCE_NOT_ACQUIRED_KHR";
+    default: error_message = "Unknown OpenCL error";
+    }
 
-   if (err >= -63 && err <= 0)
-      perror("%s", strings[-err]);
-   else
-      perror("%s", strings[64]);
+   perror(error_message);
    if(message!=NULL)
-      perror("\n%s", message);
+      perror(message);
    exit(1);
    
 }
