b9c72b8bf279ef3fe4d3a78a563b614a3302cb9a
Arxos0012 <booker.oronde@gmail.com>
2018-07-02 15:08:00 -0500

    Made corrections to reading chunk and created C++ style version

--- cppImp/helper_functions.hpp
+++ cppImp/helper_functions.hpp
@@ -2,6 +2,9 @@
 #define HELPER_FUNCTIONS
 
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <CL/cl.hpp>
 
 #define CHUNK_SIZE 1024
@@ -15,90 +18,83 @@ cl_int pad_num(cl_int old) {
    return new_val;
 }
 
-/* Copies a chuck of data from a file close to CHUNK_SIZE that ends in a '\n' */
-cl_uint read_chunck(FILE * fp, char ** chunk, char ** residual, unsigned int len){
-   cl_char c;
-   unsigned int count = len;
-   unsigned long start = ftell(fp);
+/* Reads in a chunk of data from file. Ensures that  the chunk
+   starts/ends on with a complete line.
+*/
+void read_chunk(FILE * fp, char ** chunk, char ** residual,
+               cl_uint *len, cl_uint *residual_len){
+   char c;
+   cl_uint count = *len;
    
    //copying chunk from file
    while(count < CHUNK_SIZE){
       
+      //hit the end of file
       if((c = fgetc(fp)) == EOF){
+         //getting rid of any possible ending newlines
+         while((*chunk)[count-1] == '\n'){
+            *chunk = (char *)realloc(*chunk, (--count)*sizeof(char));
+         }
          *residual = NULL;
-         return count;
+         *residual_len = 0;
+         *len = count;
+         return;
       }
 
-      *chunk = (char *)realloc(*chunk, (++count)*sizeof(cl_char));
+      *chunk = (char *)realloc(*chunk, (++count)*sizeof(char));
       (*chunk)[count-1] = c;
    }
 
    //seeking the past '\n'
    if(c != '\n'){
-      unsigned int end = count-1;
+      cl_uint chunk_size = count-1;
       while(c != '\n'){
-         --end;
-         c = (*chunk)[end];
+         --chunk_size;
+         c = (*chunk)[chunk_size];
       }
       
-      unsigned int chunk_size = count - end;
-      unsigned int res_size = count - chunk_size;
+      cl_uint res_size = count - chunk_size - 1;
       
       //copying start of next line into residual
-      *residual = (char *)malloc(sizeof(cl_char)*(res_size));
-      memcpy(*residual, (*chunk) + chunk_size, sizeof(cl_char)*(res_size));
+      *residual = (char *)malloc(sizeof(char)*(res_size));
+      memcpy(*residual, (*chunk) + chunk_size + 1, sizeof(char)*(res_size));
 
       //Getting rid of incomplete line from chunk and throwing away '\n'
-      *chunk = (char*)realloc(*chunk, sizeof(cl_char)*(chunk_size-1));
+      *chunk = (char*)realloc(*chunk, sizeof(char)*(chunk_size));
 
-      return chunk_size;
+      *len = chunk_size;
+      *residual_len = res_size;
+      return;
    }
 
    //throwing away ending '\n'
-   *chunk = (char*)realloc(*chunk, sizeof(cl_char)*(count-1));
-   return count-1;
+   *chunk = (char*)realloc(*chunk, sizeof(char)*(count-1));
+   *residual = NULL;
+   *residual_len = 0;
+   *len = count-1;
+   return;
 }
 
-cl_uint read_from_file(FILE* fp, char* line, cl_int* guess, char* eof) {
-   if(!fp) {
-      printf("Couldn't open input file");
-      exit(1);
-   }
-
-   cl_uint counter=0, size=*guess;
-   cl_char c;
-   while((c = fgetc(fp)) != '\n') {
-      if(feof(fp)) {
-         eof[0]=1;
-         break;
-      }
-      line[counter] = c;
-      counter++;
-      if(counter==size) {
-         size*=2;
-         line = (char*)realloc(line, size * sizeof(cl_char));
-         if(line==NULL) {
-            perror("Couldn't realloc line");
-            exit(1);
-         }
-      }
-
-   }
-   if(counter<size) {
-      for(int i = counter; i < size; ++i) {
-         line[i] = ' ';
-      }
-   }
-   *guess = counter;
-   *guess = pad_num(*guess);
-   
-   return size;
-
-}
-
-/* Padding string w/ spaces to a length of next power of 2.
-   Stores padded string and new length in parameters.
+/* Reads in a chunk of data from file. Ensures that  the chunk
+   starts/ends on with a complete line.
 */
+void read_chunk_pp(std::ifstream & file, std::string & chunk, std::string & residual){
+   unsigned int size = chunk.size();
+   std::string line;
+   while(std::getline(file, line)){
+      if(size + line.size() > CHUNK_SIZE){
+         //removing ending newline
+         chunk = chunk.substr(0, size-1);
+         residual = line;
+         return;
+      }
+
+      chunk += line + "\n";
+      size = chunk.size();
+   }
+}
+
+/* Padding string w/ spaces to a length of next power of 2. */
 void pad_string(char** str, cl_int* len){ 
    //returns the next power of 2 from len
    cl_int new_len = pad_num(*len);
