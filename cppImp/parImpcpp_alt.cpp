9e4d254ac532e89b96fee08da62fa1c0cd1b05af
gmaayan <gabemgem@gmail.com>
2018-07-23 16:13:15 -0500

    Removed unused buffer

--- cppImp/parImpcpp_alt.cpp
+++ cppImp/parImpcpp_alt.cpp
@@ -228,12 +228,10 @@ int main(int argc, char** argv){
    }
 
    
-   cl_mem inputString2 = clCreateBuffer(context, CL_MEM_READ_WRITE |
-            CL_MEM_COPY_HOST_PTR, chunkSize, c_chunk, &err);
-   error_handler(err, "Failed to create 'inputString' buffer2");
 
    for(size_t i=0; i<posSize; i+=2) {
       cl_int currStart = pos[i];
+      if(sizes[i/2]==0) {continue;}
       cl_uint currSize = sizes[i/2]-7; //7 irrelevant commas
       cl_uint finalSize = (pos[i+1]-commPos[currStart+7]-1);
       cl_uint* currCommas = &commPos[currStart+7];
@@ -250,7 +248,7 @@ int main(int argc, char** argv){
                            finalSize*sizeof(cl_char), NULL, &err);
       error_handler(err, "Failed to create 'output_line' buffer");
 
-      errors.push_back(clSetKernelArg(flipCoords, 0, sizeof(cl_mem), &inputString2));   //input_string
+      errors.push_back(clSetKernelArg(flipCoords, 0, sizeof(cl_mem), &inputString));   //input_string
       errors.push_back(clSetKernelArg(flipCoords, 1, sizeof(cl_mem), &startPos));      //start_positions
       errors.push_back(clSetKernelArg(flipCoords, 2, sizeof(cl_uint), &currSize));     //num_pairs
       errors.push_back(clSetKernelArg(flipCoords, 3, sizeof(cl_mem), &pos_ptr2));      //pos_ptr
@@ -260,6 +258,7 @@ int main(int argc, char** argv){
       errors.push_back(clSetKernelArg(flipCoords, 7, sizeof(cl_uint), NULL));          //mid
       errors.push_back(clSetKernelArg(flipCoords, 8, sizeof(cl_uint), NULL));          //y_len
       errors.push_back(clSetKernelArg(flipCoords, 9, sizeof(cl_mem), &output_line));   //output_string
+      errors.push_back(clSetKernelArg(flipCoords, 10, sizeof(cl_uint), &currCommas[0]));//start_location
       error_handler(errors, "Couldn't set args for flipCoords");
 
       err = clEnqueueNDRangeKernel(queue, flipCoords, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
@@ -269,13 +268,14 @@ int main(int argc, char** argv){
       err = clEnqueueReadBuffer(queue, output_line, CL_TRUE, 0, 
                   finalSize*sizeof(cl_char), output_str, 0, NULL, NULL);
       clFinish(queue);
+      
       cl_uint tag_length = commPos[currStart]-currStart+1;
-      cout<<chunk.substr(currStart, tag_length);
+      cout<<chunk.substr(currStart, tag_length)<<'\"';
       for(cl_uint i=0; i<finalSize; ++i) {
          cout<<output_str[i];
       }
       cout<<endl<<endl;
-
+      
       free(output_str);
       clReleaseMemObject(pos_ptr2);
       clReleaseMemObject(startPos);
@@ -288,7 +288,6 @@ int main(int argc, char** argv){
 
    //Freeing CL Objects
    clReleaseMemObject(inputString);
-   clReleaseMemObject(inputString2);
    clReleaseMemObject(newLineBuff);
    clReleaseMemObject(posBuff);
    clReleaseMemObject(finalRes);
